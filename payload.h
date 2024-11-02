5fbb6ef2d4449d84d79ed6b7991d8e9ad3592742
GU Junhua <jhgu@nao.cas.cn>
2024-11-02 15:04:35 +0800

    modified:   payload.h

--- payload.h
+++ payload.h
@@ -8,10 +8,12 @@
 #include "config.h"
 
 struct Payload{
-    uint64_t head=0x5555aaaa12345678;
+    uint32_t head=0x12345678;
+    uint32_t version=0xa1a1a1a1;
     uint64_t pkt_cnt=0;//8 8
     int64_t base_id=0;//8 16
     uint64_t port_id=0;//4 20
+    uint64_t npt_per_frame=N_PT_PER_FRAME;
     uint64_t _reserve=0;
     int16_t data[N_PT_PER_FRAME]={};//8192 8216
 };
